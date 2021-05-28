a1f76a64b1a14b661ee9ce16b5302ef258205f48
Okonkwo Maryann Chidimma <you@example.com>
2021-05-28 15:25:06 +0100

    added endpoint to handle category delete and update

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -1,9 +1,12 @@
 const categoriesService = require('../services/categories.service')
 const appResponse = require('../../lib/appResponse')
 const { singleUpload } = require('../../lib/cloudinary')
+const { BadRequestError } = require('../../lib/appError')
 class CategoriesController {
     create = async (req, res) => {
         const image = await singleUpload(req.file)
+        if (!image) throw new BadRequestError("image Upload failed")
+
         const data = await categoriesService.create({ ...req.body, image })
         res.status(201).send(appResponse("category created successfully", data))
     }
@@ -12,6 +15,25 @@ class CategoriesController {
         const data = await categoriesService.find()
         res.send(appResponse("all categories", data))
     }
+
+    update = async (req, res) => {
+        const data = { ...req.body }
+        if (req.file) {
+            let fileUpload = await singleUpload(req.file)
+
+            if (!fileUpload) throw new BadRequestError("image uplaod failed")
+
+            data.image = fileUpload
+        }
+        const category = await categoriesService.update(req.query.id, data)
+        res.send(appResponse("categoryupdated successfully", category))
+    }
+
+    delete = async (req, res) => {
+        const data = await categoriesService.delete(req.query.id)
+        res.send(appResponse("category has successfully been deleted"))
+    }
+
 
 
 }
