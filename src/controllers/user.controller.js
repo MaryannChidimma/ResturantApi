82480fe3b5ad29b35daaf575d901c63b1efebc07
Okonkwo Maryann Chidimma <you@example.com>
2021-05-28 10:59:26 +0100

    refactored updateUser func

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,34 +1,29 @@
 const userService = require("../services/user.service");
 const appResponse = require("../../lib/appResponse");
-const { ExpectationFailedError,BadRequestError } = require("../../lib/appError");
+const { BadRequestError } = require("../../lib/appError");
 const { singleUpload } = require('../../lib/cloudinary')
 const _ = require("lodash");
 
 class userController {
-	async getAll(req, res) {
-		const users = await userService.find();
-		res.send(appResponse("All users", users));
-	}
+    async getAll(req, res) {
+        const users = await userService.find();
+        res.send(appResponse("All users", users));
+    }
 
-	async updateUser(req, res) {
-        const { fullName, phoneNumber, address } = req.body;
-        if (!(fullName && phoneNumber && address))
-        throw new ExpectationFailedError();
-        
-        let fileUpload = null;
-        if(req.file){
-            fileUpload = await singleUpload(req.file)
+    async updateUser(req, res) {
+
+        const data = { ...req.body }
+
+        if (req.file) {
+            let fileUpload = await singleUpload(req.file)
             if (!fileUpload) throw new BadRequestError("image upload failed")
+            data.image = fileUpload
         }
-		
-	
-        const data ={ fullName, phoneNumber, address};
-        if(fileUpload) data.image = fileUpload;
 
-		const user = await userService.update(req.user._id, data );
-		res.send(appResponse("Update sucessful", user));
-	}
-    
+        const user = await userService.update(req.user._id, data);
+        res.send(appResponse("Update sucessful", user));
+    }
+
 }
 
 module.exports = new userController();
