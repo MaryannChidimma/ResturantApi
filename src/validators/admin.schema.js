39f0fcb2342ef0163bb483bef6a0e8fdd229bb67
Okonkwo Maryann Chidimma <you@example.com>
2021-05-31 22:56:12 +0100

    modified adminSignUpSchema func

--- src/validators/admin.schema.js
+++ src/validators/admin.schema.js
@@ -2,8 +2,7 @@ const Joi = require("joi");
 
 const adminSignUpSchema = Joi.object({
     fullName: Joi.string().required().min(2).max(255),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    email: Joi.string().email().required()
 });
 
 const adminLoginSchema = Joi.object({
