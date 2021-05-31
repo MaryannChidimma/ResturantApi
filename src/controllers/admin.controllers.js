8c99ccab256e0a886bcd164deadab5ae08e7f126
Okonkwo Maryann Chidimma <you@example.com>
2021-05-31 22:50:32 +0100

    modified signup function in admin cntrollers

--- src/controllers/admin.controllers.js
+++ src/controllers/admin.controllers.js
@@ -1,24 +1,37 @@
 const appResponse = require("../../lib/appResponse");
 const { BadRequestError, UnAuthorizedError } = require("../../lib/appError");
 const adminService = require("../services/admin.services");
-const { hashPassword, comparePassword, generateToken } = require("../utils/authHandler");
+const { hashPassword, comparePassword, generateAdminToken } = require("../utils/authHandler");
+const { randomPassword } = require("../utils/generatePassword")
 const _ = require('lodash')
 
 class AdminController {
     signup = async (req, res) => {
-        const existingadmin = await adminService.findByEmail(req.body.email);
-        if (existingadmin)
-            throw new BadRequestError("This email is already registered");
+        if (req.admin.type === process.env.TYPE) {
+            req.body.email = req.body.email.toLowerCase()
 
-        req.body.password = await hashPassword(req.body.password);
+            const existingadmin = await adminService.findByEmail(req.body.email);
+            if (existingadmin)
+                throw new BadRequestError("This email is already registered");
 
-        const admin = await adminService.create({ ...req.body, createdBy: req.admin._id });
-        res.send(
-            appResponse("Admin created successfully", _.omit(admin._doc, ['password']))
-        );
+            let generatePassword = randomPassword(10)
+            const password = await hashPassword(generatePassword);
+
+            const admin = await adminService.create({ ...req.body, password, createdBy: req.admin._id }, generatePassword);
+
+            await adminService.sendPasswordMail(admin, generatePassword)
+            res.send(
+                appResponse("Admin created successfully", _.omit(admin._doc, ['password']))
+            );
+        }
+        else {
+            throw new UnAuthorizedError("Admin is not authorized to carry out this operation")
+        }
     };
 
     login = async (req, res) => {
+        req.body.email = req.body.email.toLowerCase()
+
         const validAdmin = await adminService.findByEmail(req.body.email);
         if (!validAdmin) throw new UnAuthorizedError("Invalid email or password");
 
@@ -29,12 +42,19 @@ class AdminController {
         if (!validPassword)
             throw new UnAuthorizedError("Invalid email or password");
 
-        const authToken = generateToken(validAdmin);
-        const adminData = _.omit(validAdmin._doc, ['password', 'updatedAt'])
+        const authToken = generateAdminToken(validAdmin);
+        const adminData = _.omit(validAdmin._doc, ['password', 'updatedAt', 'type'])
         res.send(
             appResponse("admin login successful", { ...adminData, authToken })
         );
     };
+
+    getAll = async (req, res) => {
+        const admins = await adminService.getAll()
+
+        res.send(appResponse("all admin", admins))
+    }
+
 }
 
 module.exports = new AdminController();
