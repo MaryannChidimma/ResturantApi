483c5a8c8bdddb05ae25ccc58d564fff5e081b0b
Okonkwo Maryann Chidimma <you@example.com>
2021-06-02 16:49:07 +0100

    enabled discount

--- src/validators/menu.schema.js
+++ src/validators/menu.schema.js
@@ -6,6 +6,7 @@ const menuSchema = Joi.object({
     description: Joi.string().required().min(2).max(255),
     price: Joi.number().required(),
     category: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+    discount: Joi.string(),
 });
 
 const updateMenuSchema = Joi.object({
