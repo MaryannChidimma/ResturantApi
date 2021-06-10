0e6db5ee1d1cfaa59ef843b1f987f76f1b0b7d35
Okonkwo Maryann Chidimma <you@example.com>
2021-06-10 11:12:55 +0100

    added schema for order

---
+++ src/validators/order.schema.js
@@ -0,0 +1,38 @@
+const Joi = require("joi");
+
+
+const makeOrderSchema = Joi.object({
+    transactionId: Joi.number().required(),
+    user: Joi.string().regex(/^[0-9-FA-F]{24}$/),
+    subTotal: Joi.number(),
+    total: Joi.number(),
+    discount: Joi.number(),
+    shippingFee: Joi.number(),
+    status: Joi.string(),
+    orderItems: Joi.array().required(),
+    address: Joi.string().required(),
+    orderDate: Joi.date(),
+    deliveryDate: Joi.date(),
+    deliveryType: Joi.string(),
+
+
+});
+
+const filterOrderSchema = Joi.object({
+    user: Joi.string().regex(/^[0-9-FA-F]{24}$/),
+    menu: Joi.string().regex(/^[0-9-FA-F]{24}$/),
+    status: Joi.string(),
+    orderDate: Joi.date(),
+    deliveryDate: Joi.date(),
+
+
+})
+
+
+module.exports = {
+    makeOrderSchema,
+    filterOrderSchema,
+
+};
+
+
