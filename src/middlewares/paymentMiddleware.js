434c621ac83211e8528561917dd9926888a43ffa
Okonkwo Maryann Chidimma <you@example.com>
2021-06-10 11:14:00 +0100

    added middleware to verify flutterwave transaction

---
+++ src/middlewares/paymentMiddleware.js
@@ -0,0 +1,17 @@
+
+const { VerifyTransaction } = require("../utils/payment")
+const { BadRequestError } = require('../../lib/appError');
+
+exports.verifyPayment = async (req, res, next) => {
+    const token = Number(req.body.transactionId);
+
+    const data = await VerifyTransaction(token)
+    if (data) {
+        req.transDetails = data;
+        next();
+    }
+    else {
+        throw new BadRequestError("Something went wrong, transaction Id might be invalid")
+    }
+}
+
