65df415e96985d3efe7bef0399d57de460a1c582
Okonkwo Maryann Chidimma <you@example.com>
2021-06-10 11:13:18 +0100

    added order controller

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,63 @@
+const orderService = require('../services/order.service');
+const menuService = require('../services/menu.service')
+const { random } = require('../utils/random')
+const appResponse = require('../../lib/appResponse');
+const { BadRequestError } = require("../../lib/appError")
+
+class orderController {
+    makeOrder = async (req, res) => {
+
+        const { orderItems } = req.body
+        const orderItemsObj = {}
+        let sum = 0
+        const user = req.user._id
+
+        const orderId = random(8)
+
+        orderItems.forEach(item => {
+            orderItemsObj[item.menu] = item.quantity
+        });
+
+        const menuIds = Object.keys(orderItemsObj)
+
+        const menus = await menuService.find({
+            '_id': { $in: menuIds }
+        })
+
+        menus.forEach(menu => {
+            if (menu.discount === 0) {
+                sum += (parseFloat(menu.price) * parseFloat(orderItemsObj[menu._id]))
+            }
+            else {
+                const discountedPrice = orderService.calculateDiscount(menu.discount, menu.price)
+                let price = menu.price - discountedPrice
+                sum += (parseFloat(price) * parseFloat(orderItemsObj[menu._id]))
+            }
+        })
+        if (!sum) throw new BadRequestError("somethidng went wrong, could not compute sum")
+
+        //check if the total gotten from flutterwave transaction matches the sum
+        if (req.transDetails.amount !== sum) {
+
+            const subTotal = sum
+            const total = subTotal + Number(req.body.shippingFee)
+            const order = await orderService.makeOrder({ ...req.body, user, orderId, subTotal, total })
+            res.send(appResponse("order created successfully", order))
+
+        }
+
+        else {
+            throw new BadRequestError("invalid transaction could not make order")
+        }
+
+
+    }
+
+    getAll = async (req, res) => {
+        const orders = await orderService.find(req.query);
+        res.send(appResponse("All orders", orders))
+    }
+
+}
+
+module.exports = new orderController()
